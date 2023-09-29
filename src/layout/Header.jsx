64479fe4c1fd83020e6430beb3784cb35c3abc7b
Érick Santos <ericksantos@mailfence.com>
2023-09-29 15:24:29 -0300

    feat: adiciona responsividade para a tela de login

--- src/layout/Header.jsx
+++ src/layout/Header.jsx
@@ -1,15 +1,28 @@
-import { Menu } from "./Menu";
+import { MenuButtons } from "./MenuButtons";
 import { Link } from "react-router-dom";
 import styles from "../styles/Header.module.css";
 import { Typography } from "@mui/material";
+import MenuIcon from "@mui/icons-material/Menu";
+import { useState } from "react";
 
 export const Header = () => {
+  const [isMenuOpen, setIsMenuOpen] = useState(false);
+
+  const toggleMenu = () => {
+    setIsMenuOpen(!isMenuOpen);
+  };
+
   return (
     <header className={styles.header}>
+      <div className={styles.menuIcon} onClick={toggleMenu}>
+        <MenuIcon />
+      </div>
       <Link to="/" className={styles.header__links}>
         <Typography variant="h4">Note2Note</Typography>
       </Link>
-      <Menu></Menu>
+      <div className={styles.menu}>
+        <MenuButtons></MenuButtons>
+      </div>
     </header>
   );
 };
