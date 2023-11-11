0f3124ad9d37ef0f6fd4ee46fdaf08fbd3494ef9
Érick Santos <ericksantos@mailfence.com>
2023-11-11 10:00:47 -0300

    adiciona página de Diario

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -6,10 +6,12 @@ import { Link } from "react-router-dom";
 import Box from "@mui/material/Box";
 import Fab from "@mui/material/Fab";
 import Button from "@mui/material/Button";
+import { useNavigate } from "react-router-dom";
 import AddIcon from "@mui/icons-material/Add";
 import { useEffect, useState } from "react";
 import api from "../services/api";
 import { userStore } from "../stores/userState";
+import { format } from "date-fns";
 
 export const Home = () => {
   const [notes, setNotes] = useState([]);
@@ -17,7 +19,7 @@ export const Home = () => {
 
   const userState = userStore((state) => state.userLogged);
   const setUserState = userStore((state) => state.setUserState);
-
+  const navigate = useNavigate();
   useEffect(() => {
     api
       .get("/diarios/diarios")
@@ -30,10 +32,45 @@ export const Home = () => {
       });
   }, []);
 
+  const formatDate = (dateString) => {
+    const date = new Date(dateString);
+    return format(date, "yyyy-MM-dd");
+  };
+
+  const navigateWithParams = (param1) => {
+    navigate(`/diary/${param1}`);
+  };
+
   const buttonStyle = {
     position: "fixed",
     bottom: "20px",
     right: "20px",
+  };
+
+  const diarios = {
+    // backgroundColor: "yellow",
+    display: "flex",
+    flexWrap: "wrap",
+    gap: "20px",
+    padding: "40px",
+    justifyContent: "center",
+  };
+
+  const diario = {
+    backgroundColor: "#FFFDD0",
+    padding: "10px",
+    border: "solid 1px",
+    borderRadius: "10px",
+    cursor: "pointer",
+    width: "200px",
+  };
+
+  const diarioTitulo = {
+    fontSize: "14px",
+    fontFamily: "Arial",
+    fontWeight: "bold",
+    textAlign: "center",
+    padding: "5px",
   };
 
   let content;
@@ -97,6 +134,37 @@ export const Home = () => {
         <Typography sx={{ color: "white", textAlign: "center" }} variant="h3">
           Empty list. Create a Diary below
         </Typography>
+        <Box sx={{ "& > :not(style)": { m: 1 } }}>
+          <Fab color="primary" aria-label="add" style={buttonStyle}>
+            <Link to="/create-diary">
+              <AddIcon />
+            </Link>
+          </Fab>
+        </Box>
+      </Section>
+    );
+  } else {
+    content = (
+      <Section>
+        <div style={diarios}>
+          {notes.map((elements, index) => (
+            <div
+              key={index}
+              style={diario}
+              onClick={() => {
+                navigateWithParams(elements?.id);
+              }}
+            >
+              <p style={diarioTitulo}>{elements?.diarioNome}</p>
+              <p style={{ textAlign: "center", color: "gray" }}>
+                {elements?.diarioDescricao}
+              </p>
+              <p style={{ textAlign: "center" }}>
+                {formatDate(elements?.createdAt)}
+              </p>
+            </div>
+          ))}
+        </div>
         <Box sx={{ "& > :not(style)": { m: 1 } }}>
           <Fab color="primary" aria-label="add" style={buttonStyle}>
             <Link to="/create-diary">
