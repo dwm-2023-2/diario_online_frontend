6939f2b9b9081a8320eed10a1602eab5e76e9083
joao pedro melo <joaop.jpmp@gmail.com>
2023-12-08 01:13:03 -0300

    feat: edit-profile

--- src/pages/Profile.jsx
+++ src/pages/Profile.jsx
@@ -2,17 +2,43 @@ import { Header } from "../layout/Header";
 import { Section } from "../layout/Section";
 import { Footer } from "../layout/Footer";
 import { userInfoStore } from "../stores/userInfo";
+import api from "../services/api";
 import styles from "../styles/Profile.module.css";
+import { useNavigate } from "react-router-dom";
 import { Typography, TextField, Button } from "@mui/material";
+
+// ... (importações existentes)
 
 export const Profile = () => {
   const userInfoState = userInfoStore((state) => state.userInfo);
+  const navigate = useNavigate();
 
   let storageUserId = localStorage.getItem("userId");
   let storageUsername = localStorage.getItem("username");
   let storageEmail = localStorage.getItem("email");
 
-  const submitSignup = () => {};
+  const submitDelete = () => {
+    const shouldDelete = window.confirm(
+      "Are you sure you want to delete your account?"
+    );
+    if (shouldDelete) {
+      api
+        .delete(`/users/${storageUserId}`)
+        .then((response) => {
+          console.log(response);
+          navigate("/");
+          localStorage.clear();
+        })
+        .catch((err) => {
+          console.error("Oops! An error occurred" + err);
+        });
+    }
+  };
+
+  const handleEdit = () => {
+    // continuar a lógica
+    navigate(`/edit-profile/${storageUserId}`);
+  };
 
   return (
     <div>
@@ -25,21 +51,17 @@ export const Profile = () => {
             <p>User ID: {storageUserId}</p>
             <p>Email: {storageEmail}</p>
             <div className={styles.buttons}>
-              <Button
-                onClick={() => {
-                  navigate("/editprofile");
-                }}
-                variant="contained"
-              >
-                Login
+              <Button onClick={handleEdit} variant="contained" color="primary">
+                Editar perfil
               </Button>
               <Button
                 onClick={() => {
                   submitDelete();
                 }}
                 variant="contained"
+                color="error"
               >
-                Create
+                Deletar usuário
               </Button>
             </div>
           </div>
